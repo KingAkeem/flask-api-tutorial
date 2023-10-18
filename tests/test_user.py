e3e91bd265e1b3679d53c5b4123a68044c10ac31
Akeem King <akeemtlking@gmail.com>
2023-10-18 11:21:39 -0400

    Add user login test

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,3 +11,15 @@ def test_register() -> None:
         response_data = response.get_json()
         assert response_data["message"] == "User created successfully."
         assert response.status_code == HTTPStatus.CREATED
+
+
+def test_login() -> None:
+    flask_app = create_app()
+
+    with flask_app.test_client() as client:
+        test_json = {"username": "test-user", "password": "test-password"}
+        response = client.post("/login", json=test_json)
+        response_data = response.get_json()
+        assert "access_token" in response_data
+        assert "refresh_token" in response_data
+        assert response.status_code == HTTPStatus.OK
